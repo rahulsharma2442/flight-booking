b901da7ec8d0958c63b413f40e1aafce1a087deb
rahul-sharma24 <rahul2003shukla@gmail.com>
2025-01-16 17:15:05 +0530

    added file to gitignore

--- src/modles/user.js
+++ src/modles/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-<<<<<<< HEAD
 require('dotenv').config({ path: '../config/secrets.env' });
 
 const userSchema = new mongoose.Schema(
@@ -73,38 +72,3 @@ userSchema.methods.getJWT = async function () {
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
-=======
-const userSchema = new mongoose.Schema({
-    firstName: { type: String,required:true,trim:true ,minLength:3,maxLength:20},
-    lastName: { type: String ,trim:true},
-    email: { type: String, unique: true, required: true ,lowercase:true,trim:true,
-        validate(val){
-            if(!validator.isEmail(val)){
-                throw new Error ("Email  is not valid");
-            }
-        }
-    },
-    password: { type: String, required: true },
-    age: { type: Number,min:14 },
-    gender: { type: String,trim:true,lowercase:true,
-        validate(val){
-            if(!["male","female","other"].includes(val)){
-                throw new Error("Gender is not valid");
-            }
-        }
-     } ,
-    nationality:{type:String,default:"Indian",trim:true},
-    skills:{type:Array},
-},{
-    timestamps:true
-});
-userSchema.methods.getJWT = async function() {
-    const user = this;
-    const token = jwt.sign({ _id: user._id }, "krishna", { expiresIn: "24h" });
-    return token;
-};
-
-
-const User = mongoose.model('User', userSchema);
-module.exports = {User}
->>>>>>> 38c2a54817f883f54bb8d718ca32f6da0eb6aac2
